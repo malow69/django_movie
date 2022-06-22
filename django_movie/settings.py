ae8b9da7606c07849153f35763c05e9e0d460681
malow69 <magomed.umd@gmail.com>
2022-06-22 23:13:01 +0300

    change static base_dir #3

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [STATIC_DIR]
 #    BASE_DIR / "static",
 #]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
